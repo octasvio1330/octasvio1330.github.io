efd66d10a5491f1de79c0fd8094c6ca62cfe13eb
octasvio1330 <74885967+octasvio1330@users.noreply.github.com>
2021-03-21 19:41:57 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; 2020 Ricardo Armando Machorro Reyes.`;
+      `Copyright &copy; 2021 Mancilla Cordero Octavio`;
   }
 }
 customElements.define("mi-footer", MiFooter);
